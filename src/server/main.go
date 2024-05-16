d232908142b07fda784d4c46c08daa9263997b48
Maciej Gierada <maciek.gierada@gmail.com>
2024-05-16 10:40:42 +0200

    feat: add basic setup of the http server

---
+++ src/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+	var port int = 3333
+	var host string = "localhost"
+	addr := fmt.Sprintf("%s:%d", host, port)
+	fmt.Printf("🚀 Starting server on %s...\n", addr)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+
+}
