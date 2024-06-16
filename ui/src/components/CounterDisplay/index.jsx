6452322c968f538ba8c37799eb1447b6c783dd44
Maciej Gierada <maciek.gierada@gmail.com>
2024-06-16 11:36:01 +0200

    feat: get rid of card and simplify styling

--- ui/src/components/CounterDisplay/index.jsx
+++ ui/src/components/CounterDisplay/index.jsx
@@ -2,14 +2,15 @@
 
 const CounterDisplay = ({ currentValue, error }) => {
   return (
-    <div>
+    <div className="flex items-center justify-center h-full text-center">
       {error ? (
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           {error}
         </h1>
       ) : (
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Current streak: {currentValue} days 🔥
+          Current streak:
+          <br /> {currentValue} days 🔥
         </h1>
       )}
     </div>
