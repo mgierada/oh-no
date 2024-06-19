87db1997d32c77f485536b44f071eb7f69a6e898
Maciej Gierada <maciek.gierada@gmail.com>
2024-06-19 09:56:54 +0200

    feat: hooked up max value logic in ui with backedn

--- ui/src/app/page.js
+++ ui/src/app/page.js
@@ -6,6 +6,7 @@ import HealthStatus from "@/components/HealthStatus";
 /**
  * @typedef {Object} CounterApiResponse
  * @property {number} CurrentValue - The current value of the counter.
+ * @property {number} MaxValue - the maximum value of the counter recorded so far.
  * @property {string} UpdatedAt - The timestamp when the counter was last updated.
  * @property {Object} ResetedAt - The reset information.
  * @property {string} ResetedAt.String - The timestamps when the counter was reset.
@@ -16,6 +17,7 @@ import HealthStatus from "@/components/HealthStatus";
 /**
  * @typedef {Object} Counter
  * @property {number|null} currentValue - The current value of the counter.
+ * @property {number|null} maxValue - The max value of the counter recorded so far.
  * @property {string|null} updatedAt - The timestamps when the last update was made.
  * @property {boolean} isLocked - A flag indicating whether the counter updates are currently locked
  * @property {string|null} resetedAt - The timestamps when the counter was reset.
@@ -27,12 +29,12 @@ import HealthStatus from "@/components/HealthStatus";
  * Fetches the current value of the counter from the API.
  * @returns {Promise<Counter>} A promise that resolves to an object containing the current value and an error message if any.
  */
-const fetchCounter = async () => {
+const fetchCounter = async (endpoint) => {
   try {
     const rootUrl = process.env.NEXT_PUBLIC_ROOT_API_URL;
-    const endpoint = `${rootUrl}/counter`;
+    const url = `${rootUrl}/${endpoint}`;
 
-    const response = await fetch(endpoint, { cache: "no-store" });
+    const response = await fetch(url, { cache: "no-store" });
 
     if (!response.ok) {
       throw new Error("Failed to fetch data");
@@ -48,6 +50,7 @@ const fetchCounter = async () => {
     /** @type {Counter} */
     const result = {
       currentValue: data.CurrentValue,
+      maxValue: data.MaxValue,
       updatedAt: data.UpdatedAt,
       resetedAt: data.ResetedAt.String,
       wasEverReset: data.ResetedAt.Valid,
@@ -59,6 +62,7 @@ const fetchCounter = async () => {
     /** @type {Counter} */
     return {
       currentValue: null,
+      maxValue: null,
       updatedAt: null,
       resetedAt: null,
       wasEverReset: null,
@@ -74,29 +78,37 @@ const fetchCounter = async () => {
  */
 const Home = async () => {
   /** @type {Counter} */
-  const data = await fetchCounter();
+  const dataHealthyCounter = await fetchCounter("counter");
+  /**@type {Counter} */
+  const dataIllnessCounter = await fetchCounter("ohno-counter");
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-between p-4">
       <div className="flex space-x-4 justify-center lg:grid-cols-2 md:grid-cols-2">
         <DisplayCard
           title="Healthy"
-          value={26}
-          description="better then last time"
+          value={dataHealthyCounter.maxValue}
+          description="Maximum number of days without illness"
         />
         <DisplayCard
           title="Illness"
-          value={2}
-          description="worse then last time"
+          value={dataIllnessCounter.maxValue}
+          description="Maximum duration of the illness in days"
         />
       </div>
       <div className="flex flex-col items-center justify-center">
-        <HealthStatus isLocked={data.isLocked} error={data.error} />
-        <CounterDisplay currentValue={data.currentValue} error={data.error} />
+        <HealthStatus
+          isLocked={dataHealthyCounter.isLocked}
+          error={dataHealthyCounter.error}
+        />
+        <CounterDisplay
+          currentValue={dataHealthyCounter.currentValue}
+          error={dataHealthyCounter.error}
+        />
         <Callendar
           className="mt-5"
-          lastTimeReseted={data.resetedAt}
-          currentCouterValue={data.currentValue}
+          lastTimeReseted={dataHealthyCounter.resetedAt}
+          currentCouterValue={dataHealthyCounter.currentValue}
         />
       </div>
       <div className="flex flex-col items-center justify-center"></div>
