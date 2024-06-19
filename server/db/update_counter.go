7cfdc64cf7f1b5ee374b4b3adb171a1604dc93e8
Maciej Gierada <maciek.gierada@gmail.com>
2024-06-19 15:45:39 +0200

    fix: cleanup

--- server/db/update_counter.go
+++ server/db/update_counter.go
@@ -113,7 +113,6 @@ func upsertCounterData(tableName string) (bool, error) {
 
 		updateInterval := time.Duration(updateIntervalInt)
 
-		// if time.Since(lastUpdated) < updateInterval*time.Second {
 		if time.Since(lastUpdated) < updateInterval*time.Hour {
 			log.Printf("🙅 %d hours have not passed since the last update. Counter not increased...", updateIntervalInt)
 			return false, nil
