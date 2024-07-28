d7ac44d0e90316976e91072307ce0e913a9e8d08
Maciej Gierada <maciek.gierada@gmail.com>
2024-07-28 10:50:39 +0200

    feat: use gin

--- server/server/main.go
+++ server/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
@@ -12,31 +13,39 @@ import (
 
 func main() {
 	db.Connect()
-	http.HandleFunc("/ohno", handlers.RecordOhNoEvent)
-	http.HandleFunc("/", handlers.RedirectToCounter)
-	http.HandleFunc("/fine", handlers.RecordFineEvent)
-	http.HandleFunc("/historical/counter", handlers.GetHistoricalCounter)
-	http.HandleFunc("/historical/ohno-counter", handlers.GetHistoricalOhnoCounter)
-	http.HandleFunc("/counter", handlers.GetCounter)
-	http.HandleFunc("/ohno-counter", handlers.GetOhnoCounter)
-	http.HandleFunc("/start-incr", handlers.StartAutoUpdateCounter)
-	http.HandleFunc("/stop-incr", handlers.StopAutoUpdateCounter)
-	http.HandleFunc("/increment", handlers.IncrementCounter)
-	http.HandleFunc("/manual-increment", handlers.SetCounterValue)
-	port := os.Getenv("PORT")
-	host := os.Getenv("HOST")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// http.HandleFunc("/ohno", handlers.RecordOhNoEvent)
+	// http.HandleFunc("/", handlers.RedirectToCounter)
+	// http.HandleFunc("/fine", handlers.RecordFineEvent)
+	// http.HandleFunc("/historical/counter", handlers.GetHistoricalCounter)
+	// http.HandleFunc("/historical/ohno-counter", handlers.GetHistoricalOhnoCounter)
+	// http.HandleFunc("/counter", handlers.GetCounter)
+	// http.HandleFunc("/ohno-counter", handlers.GetOhnoCounter)
+	// http.HandleFunc("/start-incr", handlers.StartAutoUpdateCounter)
+	// http.HandleFunc("/stop-incr", handlers.StopAutoUpdateCounter)
+	// http.HandleFunc("/increment", handlers.IncrementCounter)
+	// http.HandleFunc("/manual-increment", handlers.SetCounterValue)
 
-	addr := fmt.Sprintf("%s:%s", host, port)
-	log.Print("🏗️  Starting the server...")
-	log.Printf("🚀 Listening on %s\n", addr)
-
-	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
-
-	if errors.Is(err, http.ErrServerClosed) {
-		log.Printf("server closed\n")
-	} else if err != nil {
-		log.Fatalf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
+	// port := os.Getenv("PORT")
+	// host := os.Getenv("HOST")
+	//
+	// addr := fmt.Sprintf("%s:%s", host, port)
+	// log.Print("🏗️  Starting the server...")
+	// log.Printf("🚀 Listening on %s\n", addr)
+	//
+	// err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	//
+	// if errors.Is(err, http.ErrServerClosed) {
+	// 	log.Printf("server closed\n")
+	// } else if err != nil {
+	// 	log.Fatalf("error starting server: %s\n", err)
+	// 	os.Exit(1)
+	// }
 
 }
